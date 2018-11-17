24d25c0a36297c2becd76c171c1dbba488fc3748
mist12bb <35643553+mist12bb@users.noreply.github.com>
2018-11-17 20:24:49 +0200

    Add files via upload

---
+++ precache-manifest.dcdc793e34d619bc42937f5b0ace98c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2d50f38275ffda44531f",
+    "url": "/static/js/main.2d50f382.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "2d50f38275ffda44531f",
+    "url": "/static/css/main.deba87a1.chunk.css"
+  },
+  {
+    "revision": "b2ae7638f33117a230feb6fc4ea6cefe",
+    "url": "/index.html"
+  }
+];
